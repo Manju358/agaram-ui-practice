b320c7045da98e25a2621ea9618e54edf6b63624
Manju358 <manjurajkumar98@gmail.com>
2024-11-26 16:19:54 +0530

    Resume

---
+++ 26-11-24/main.js
@@ -0,0 +1,107 @@
+let resume =
+{
+    personal_details:{
+        name            : "R.Manjula",
+        email           : "manjurajkumar98@gmail.com",
+        address         : "41, pathalvillai,Nagercoil",
+        phone           : 9942332666,
+        age             : 26,
+        dob             : "24-05-1998",
+        gender          : "Female",
+        father_name     : "E.Rajkumar",
+        mother_name     : "R.Chitradevi",
+        nationality     : "Indian",
+        marital_status  : "Married",
+    }, 
+    languages_known : [
+        "Tamil",
+        "English",
+    ], 
+    education:[
+        {
+            course     : "B.E",
+            year       : "(2015-2019)",
+            institute  : "Francis Xavier Enginerring College",
+            percentage : 75
+
+        },
+        {
+            course     : "HSC",
+            year       : "(2014-2015)",
+            institute  : "Victoria girls higher secondary school",
+            percentage : 74
+
+        },
+        {
+            course     : "B.E",
+            year       : "(2012-2013)",
+            institute  : "Victoria girls higher secondary school",
+            percentage : 88
+
+        }
+    ],
+    objective: "Seeking a challenging position with a reputable organisation where I can acquire new skills, broaden my knowledge, and use what I've learned",
+
+    skills:[
+        "HTML",
+        "CSS",
+        "Bootstrap",
+    ],
+    hobbies:[
+        "Reading",
+        "Sewing",
+        "Painting",
+    ],
+    project:[
+        {
+            title      : "Automated Traffic Controller using IOT",
+            Company    : "Digital Agility",
+            year       : "Jan-2024",
+            description: "For smart traffic control, IoT sensors are installed in various vehicles. These sensors are integrated with traffic signals, street lights, etc. Sensors facilitate continuous monitoring and help reduce accidents, making them vital for intelligent traffic management",
+        },
+        {
+            name       : "KOL Management in Pharma",
+            Company    : "Noevos Market Research Analyst Private Limited",
+            year       : "((Feb-2022)-(Nov-2023))",
+            description: "Key opinion leaders (KOLs) are external experts, such as healthcare providers, insurance professionals, patients, caregivers, and others who can share valuable expertise, opinions, and perspectives in managing a disease state",
+        }
+        
+    ],
+    work_experience:[
+        {
+            designation :"Software Tester",
+            Company     :"Digital Agility",
+            year        :"(Dec(2023)-May(2024))",
+        }, 
+        {
+            designation :"Information Analyst",
+            Company     :"Noevos Market Research Analyst Private Limited",
+            year        :"(Feb(2022)-Nov(2023))",
+        }  
+    ],
+    certification:[ 
+      {
+        course      :"Full Stack Developer",
+        institute   :"Agaram Academy",
+        year        :2024,
+        duration    :"(Nov-Mar)",
+        place       :"Nagercoil"
+      }  
+    ],   
+    declaration:"I hereby declare that the facts given above are genuine to the best od my knowledge",
+
+    date: "25-11-2024",
+    place: "Nagercoil"
+}
+console.log(resume.project[1]); 
+    
+
+        
+   
+
+        
+
+    
+
+   
+   
